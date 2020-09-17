502f22b0dd7fae7ef3c2cd8b3c73b1867934022c
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-17 13:40:45 +0200

    Added customised parameters for the DB in 'Article' model

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    /*
+        There is a lot of functionalities already available thanks to 
+        the inheritance from parent class 'Model'.
+    */
+
+    // If we wanted to change de default table name in the database.
+    protected $table = "articles";
+
+    // Primary key (if we wanted to change it)
+    public $primaryKey = 'id';
+
+    // Timestamps (set to "true" by default)
+    public $timestamps = true;
 }

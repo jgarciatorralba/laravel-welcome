c8c374e9efd83be2d85326483330fe322881a54e
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-18 00:13:19 +0200

    Added buttons to edit (<a>) and delete (<form>) an article

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -15,8 +15,19 @@
       <div class="mt-3">
         <small>
           <p class="mb-1">Written on {{$article->created_at}}</p>
-          <p class="mb-0">Last modified on {{$article->updated_at}}</p>
+          <p class="mb-3">Last modified on {{$article->updated_at}}</p>
         </small>
+      </div>
+      <hr class="my-0">
+      <div class="mt-3">
+        <a class="btn btn-primary" href="/articles/{{$article->slug}}/edit">
+          Edit
+        </a>
+        <form class="d-inline" action="/articles/{{$article->id}}" method="POST">
+          <input type="hidden" name="_method" value="DELETE">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <input type="submit" value="Delete" class="btn btn-secondary">
+        </form>
       </div>
     </div>
   </div>

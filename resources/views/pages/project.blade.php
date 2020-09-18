8381379ce09c6d9e7827a2b4469e12fd57db150b
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-18 16:03:44 +0200

    Re-worked view 'project' to center text elements

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -3,8 +3,8 @@
 @section('content')
   <div class="wrapper d-flex justify-content-center align-items-center align-self-center flex-nowrap">
     <div class="jumbotron my-0 mx-3">
-      <h1 class="my-3">Welcome to the project "{{ config('app.name') }}"</h1>
-      <p>This is a Laravel application build as a demo to get to know the framework for the first time</p>
+      <h1 class="my-3 text-center">Welcome to the project "{{ config('app.name') }}!"</h1>
+      <p class="text-center">This is a Laravel application built as a demo to get to know the framework.</p>
       <p class="text-center"><a class="btn btn-primary btn-lg" href="/" role="button">Back</a></p>
     </div>
   </div>

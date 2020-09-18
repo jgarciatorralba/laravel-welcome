e00d9dfffbf352e752bb3c92f4750fdc8a128815
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-18 16:06:44 +0200

    Fixed typo (wrong order of "!")

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="wrapper d-flex justify-content-center align-items-center align-self-center flex-nowrap">
     <div class="jumbotron my-0 mx-3">
-      <h1 class="my-3 text-center">Welcome to the project "{{ config('app.name') }}!"</h1>
+      <h1 class="my-3 text-center">Welcome to the project "{{ config('app.name') }}"!</h1>
       <p class="text-center">This is a Laravel application built as a demo to get to know the framework.</p>
       <p class="text-center"><a class="btn btn-primary btn-lg" href="/" role="button">Back</a></p>
     </div>

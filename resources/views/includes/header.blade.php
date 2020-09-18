5bdb234acc264ce087113d27b9da859c4d7d0328
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-18 21:08:01 +0200

    Re-worked navbar and nav-links

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-top border border-bottom">
     <div class="container">
         <a class="text-muted navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            Laravel
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
@@ -10,6 +10,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
+                {{-- <li class="nav-item">
+                    <a id="home" class="nav-link" href="/">
+                        Home
+                    </a>
+                </li> --}}
                 @guest
                     <li class="nav-item">
                         <a id="project" class="nav-link" href="/project">

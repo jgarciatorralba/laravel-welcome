91db7b256ead13e4ac22921e4fb7124fcc34137c
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-18 08:23:29 +0200

    Re-worked footer text

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer mt-auto py-3 fixed-bottom bg-white border border-top ">
   <div class="container text-center">
-    <span class="text-muted">&copy; Assembler School 2020.</span>
+    <span class="text-muted">&copy; Assembler School 2020</span>
   </div>
 </div>

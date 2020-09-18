c5a7d3a85a69a19e5c0dcc8aa63ee258c2216dff
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-18 15:22:27 +0200

    Added items to navbar logic + compiled file

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -1,5 +1,5 @@
 const url = window.location.href;
-const items = ['articles', 'login', 'register'];
+const items = ['project', 'contact', 'articles', 'login', 'register'];
 
 items.forEach(item => {
     if (url.includes(item)) {

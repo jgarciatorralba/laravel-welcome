39c0642cf57706a318d58bdb352eb553ff977472
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-19 17:07:35 +0200

    Added new model for table 'visits'

---
+++ app/Visit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Visit extends Model
+{
+    //
+    protected $table = 'visits';
+    
+    public $timestamps = false;
+}

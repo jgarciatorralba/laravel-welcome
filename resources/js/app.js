1ede3af8cea9b20214b9d49cb11dc1402c942604
jgarciatorralba <jgarciatorralba@gmail.com>
2020-09-20 13:44:02 +0200

    Re-worked file 'app.js' and re-compiled version

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
+// Import Bootstrap
 require('./bootstrap');
 
-// Custom JS files
+// Import other JS files
 require('./navbar');
 require('./Chart');
 require('./myChart');
+
+// Additional custom JS code
+$('.alert')
+    .delay(2000)
+    .fadeOut(2000);
